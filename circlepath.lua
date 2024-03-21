ed0cfbc635664d6b485ebb8480cc2e0681e9f36f
RAidanM <r.aidan.mealey@gmail.com>
2024-03-21 19:47:51 -0400

    circlepath program

---
+++ circlepath.lua
@@ -0,0 +1,18 @@
+---makes turtle move in a circle
+---@param cw boolean
+---@param r integer
+function circle(cw,r)
+
+    for i = 1, 4, 1 do
+        for j = 1, r, 1 do
+            turtle.forward()
+        end
+        if cw then
+            turtle.turnRight()
+        else
+            turtle.turnLeft()
+        end
+    end
+end
+
+circle(args[1], args[2])
