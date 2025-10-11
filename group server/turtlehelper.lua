68a379f9b29b5079f44186e9c0ef56e2e0ffb56c
RAidanM <r.aidan.mealey@gmail.com>
2025-10-11 14:08:27 -0400

    Calibration added

--- group server/turtlehelper.lua
+++ group server/turtlehelper.lua
@@ -30,4 +30,75 @@ function TurtleHelper.mineTo(origin, destination)
     
 end
 
+
+
+function TurtleHelper.checkIf(desired_block,pitch)
+    --reading block info from correct pitch
+    local has_block, block_info
+    if pitch == nil or pitch == "forward" then
+        has_block, block_info = turtle.inspect()
+    elseif pitch == "up" then
+        has_block, block_info = turtle.inspectUp()
+    elseif pitch == "down" then
+        has_block, block_info = turtle.inspectDown()
+    end
+    --seeing if block matches desired
+    if has_block then
+        local block_name = block_info.name
+        if string.find(block_name,desired_block) ~= nil then
+            return true
+        end
+    end
+    return false
+end
+
+---@param desired_block string
+---@return boolean
+function TurtleHelper.spinFor(desired_block)
+    for i = 1, 4, 1 do
+        if TurtleHelper.checkIf(desired_block) then
+            return true
+        elseif (desired_block=="empty") then
+            return true
+        end
+        assert(turtle.turnRight())
+    end
+    return false
+end
+
+-- might break in fluids (water, lava)
+function TurtleHelper.calibrate()
+    local turtle_loc = Coordinate.new(gps.locate())
+    local turtle_new_loc
+
+    if TurtleHelper.spinFor("empty") then
+        assert(turtle.forward())
+        turtle_new_loc = Coordinate.new(gps.locate())
+        resultant = turtle_new_loc - turtle_loc
+        if (resultant.z == -1) then
+            print("Facing North")
+            assert(turtle.back())
+            return 0
+        elseif (resultant.x == 1) then
+            print("Facing East")
+            assert(turtle.back())
+            return 1
+        elseif (resultant.z == 1) then
+            print("Facing South")
+            assert(turtle.back())
+            return 2
+        elseif (resultant.x == -1) then
+            print("Facing West")
+            assert(turtle.back())
+            return 3
+        else 
+            print("calibrattion failed")
+        end
+        
+    else
+        print("Can't calibrate. Surrounded by blocks")
+    end
+    
+end
+
 return TurtleHelper
