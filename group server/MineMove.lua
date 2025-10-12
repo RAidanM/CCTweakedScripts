6aff77643d85bbb5518bf29c799d7de3e4f48d6c
RAidanM <r.aidan.mealey@gmail.com>
2025-10-12 01:07:07 -0400

    Made independent program for MineMove

---
+++ group server/MineMove.lua
@@ -0,0 +1,8 @@
+-- required library
+local TurtleHelper = require("turtlehelper")
+
+-- arguments
+TurtleHelper.calibrate()
+TurtleHelper.mineMove(arg[1],arg[2],arg[3])
+
+print("Reached destination!")
