3370abf1e2812bc8bcb029a36df5394bff940c2d
RAidanM <r.aidan.mealey@gmail.com>
2025-10-20 19:09:37 -0400

    Added Fuel Requirement

--- Programs/MineCube.lua
+++ Programs/MineCube.lua
@@ -26,10 +26,7 @@ else
     end_coordinate = extrema_one
 end
 
---print("Travelling to " .. start_coordinate) points to table index 
-TurtleHelper.mineTo(turtle_loc, start_coordinate)
 
---mine area
 local resultant = end_coordinate - start_coordinate
 
 local levels = math.abs(resultant.y) + 1
@@ -41,50 +38,70 @@ local side = (resultant.x >= 0) and 1 or -1
 
 local lap = resultant.z
 
---reference for layers
-local i = 0
-while i < levels do
-    if i + 2 < levels then --use corridor at i+1
 
-        -- moves to correct level
-        while turtle_level < i+1 do
+-- check fuel levels
+local fuel_needed = 0
+local initial_resultant = turtle_loc - start_coordinate
+fuel_needed = initial_resultant.x + initial_resultant.y + initial_resultant.z --fuel to get to start
+            + resultant.y + ( (resultant.y % 3) * resultant.z * resultant.x ) --fuel to mine single paths
+            + ( math.floor(resultant.y / 3) * resultant.z * resultant.x ) --fuel to mine corridors
+local turtle_fuel = turtle.getFuelLevel()
+print("This job with use " .. fuel_needed .. " fuel")
+print("Turtle has " .. turtle_fuel .. " fuel")
+
+if turtle_fuel >= fuel_needed then
+    print("Turtle has enough fuel")
+    --print("Travelling to " .. start_coordinate) points to table index 
+    TurtleHelper.mineTo(turtle_loc, start_coordinate)
+
+    --reference for layers
+    local i = 0
+    while i < levels do
+        if i + 2 < levels then --use corridor at i+1
+
+            -- moves to correct level
+            while turtle_level < i+1 do
+                TurtleHelper.mineMove(0,updown,0)
+                turtle_level = turtle_level + 1
+            end
+            
+            -- mines out corridor area
+            TurtleHelper.corridorMove(0,0,lap)
+            for i = 0, distance, 1 do
+                lap = lap * -1
+                TurtleHelper.corridorMove(side,0,lap)
+            end
+            side = side * -1
+
+            --moves to end of cleared area
             TurtleHelper.mineMove(0,updown,0)
             turtle_level = turtle_level + 1
-        end
-        
-        -- mines out corridor area
-        TurtleHelper.corridorMove(0,0,lap)
-        for i = 0, distance, 1 do
-            lap = lap * -1
-            TurtleHelper.corridorMove(side,0,lap)
-        end
-        side = side * -1
 
-        --moves to end of cleared area
-        TurtleHelper.mineMove(0,updown,0)
-        turtle_level = turtle_level + 1
+            i = i + 3
+        else --use path at i
 
-        i = i + 3
-    else --use path at i
+            -- moves to correct level
+            while turtle_level < i do
+                TurtleHelper.mineMove(0,updown,0)
+                turtle_level = turtle_level + 1
+            end
+            
+            -- mines out corridor area
+            TurtleHelper.mineMove(0,0,lap)
+            for i = 0, distance, 1 do
+                lap = lap * -1
+                TurtleHelper.mineMove(side,0,lap)
+            end
+            side = side * -1
 
-         -- moves to correct level
-        while turtle_level < i do
-            TurtleHelper.mineMove(0,updown,0)
-            turtle_level = turtle_level + 1
+            i = i + 1
         end
-        
-        -- mines out corridor area
-        TurtleHelper.mineMove(0,0,lap)
-        for i = 0, distance, 1 do
-            lap = lap * -1
-            TurtleHelper.mineMove(side,0,lap)
-        end
-        side = side * -1
-
-        i = i + 1
+        lap = lap * -1
     end
-    lap = lap * -1
+else
+    print("Turtle doesn't have enough fuel")
 end
+
 
 
 
